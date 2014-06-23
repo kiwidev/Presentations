bff9cb51f1d6139400f8e03314ae19068919814f
Mark Young <mark@developer.geek.nz>
2014-06-23 20:46:08 +1200

    Adding CanopyTest

---
+++ FSharpTheEasyBits/Code/CanopyTest/CanopyTest/BasicTest.fs
@@ -0,0 +1,14 @@
+﻿module BasicTest
+
+open canopy
+open runner
+
+let basic _ =
+    "can open google" &&& fun _ ->
+        url "http://www.google.com"
+        (element "input[type='text']") << "Christchurch"
+        click "button[aria-label='Google Search']"
+        (elements "h3.r a") |> List.toArray |> (fun x -> x.[1]) |> click
+
+let all _ =
+    basic()
---
+++ FSharpTheEasyBits/Code/CanopyTest/CanopyTest/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+module Program
+
+open canopy
+open runner
+
+[<EntryPoint>]
+let main argv = 
+    start firefox
+
+    BasicTest.all()
+
+    run()   
+
+    printfn "press [enter] to exit"
+    System.Console.ReadLine() |> ignore
+
+    quit()
+    0
