c97e8bfd993e76f266d4039602c616fb5fb0a001
Mark Young <mark@developer.geek.nz>
2014-06-23 21:50:06 +1200

    Adding SqlClient Type Provider

---
+++ FSharpTheEasyBits/Code/TypeProviderTest/SqlClient.fsx
@@ -0,0 +1,27 @@
+﻿#r """packages\FSharp.Data.SqlClient.1.2.22\lib\net40\FSharp.Data.SqlClient.dll"""
+
+
+open FSharp.Data
+
+[<Literal>]
+let connectionString = @"Data Source=(local);Initial Catalog=Northwind;Integrated Security=true;"
+
+
+[<Literal>]
+let query = "
+   SELECT TOP 10 *
+   FROM [dbo].[Categories]
+"
+
+type SalesPersonQuery = SqlCommandProvider<query, connectionString>
+
+let cmd = new SalesPersonQuery()
+
+ 
+
+cmd.AsyncExecute()
+    |> Async.RunSynchronously
+    |> Seq.toList
+    |> List.length
+
+ 
